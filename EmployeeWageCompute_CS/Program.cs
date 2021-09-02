30db6d15c9ae80f216c1e17e33d8f82ff838e516
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-02 19:35:53 +0530

    [Dipti]Added usercase-1 to check employee attendance

---
+++ EmployeeWageCompute_CS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCompute_CS
+{
+    class EmployeeWageCompute
+    {
+        static void Main(string[] args)
+        {
+            //UC1 Employee Wage
+
+            //Constants
+            int IS_PRESENT = 1;
+
+            //Predefined random class for generating random values
+            Random random = new Random();
+
+            //Next method() 0-initial value,2-number of element from 0
+            int empCheck = random.Next(0, 2);
+            Console.WriteLine("Checking employee status" + empCheck);
+
+            //Selection statement
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadLine();
+        }
+    }
+}
