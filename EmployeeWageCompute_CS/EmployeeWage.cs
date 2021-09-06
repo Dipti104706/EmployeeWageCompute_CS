1a0ab43a1e5ba3a4c69b294da23a8c8f1c4890b4
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-06 22:51:07 +0530

    [Dipti] added usercase8 to calculate employee wage for multiple company

--- EmployeeWageCompute_CS/EmployeeWage.cs
+++ EmployeeWageCompute_CS/EmployeeWage.cs
@@ -6,12 +6,9 @@ namespace EmployeeWageCompute_CS
 /// </summary>
     class EmployeeWageCompute
     {
-        //UC7 Refactor the code to write method for Employee Wage Calculation for a month
+        //UC8 to multiple company Employee Wage Calculation for a month
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_WORKING_HRS = 100;
 
         //Creating method for checking employee attendance
         public static int empAttendance()
@@ -22,7 +19,7 @@ namespace EmployeeWageCompute_CS
         }
 
         //creating method to calculate employee wage
-        public static void empWage()
+        public static void empWage(string company, int empRatePerHr,int numOfWorkingDays, int maxWorkingHrs)
         {
             //local variables
             int empHrs = 0;
@@ -30,8 +27,7 @@ namespace EmployeeWageCompute_CS
             int totalEmpHrs = 0;
             int totalWorkingDays = 1;
             
-
-            while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays <= NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= maxWorkingHrs && totalWorkingDays <= numOfWorkingDays)
             {
                 totalWorkingDays++;
                 int empCheck = empAttendance();
@@ -52,14 +48,15 @@ namespace EmployeeWageCompute_CS
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs: " + empHrs);
             }
-            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total employee wage:" + totalEmpWage);
+            totalEmpWage = totalEmpHrs * empRatePerHr;
+            Console.WriteLine("Total employee wage for {0} company is {1}:" , company, + totalEmpWage);
         }
 
         static void Main(string[] args)
         {
-            EmployeeWageCompute.empWage();
-            Console.ReadLine();
+            empWage("cognizant",40,20,80);
+            empWage("Accenture", 38, 20, 85);
+            Console.ReadLine();   
         }
     }
 }
